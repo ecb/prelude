a9d46aa4a41a63131afc1a8a3101f1adcc07c334
Eric Baur <ecb@robeybaur.org>
2016-03-02 22:15:00 -0500

    get rid of uber-annoying whitespace coloring in go-mode

--- personal/go-setup.el
+++ personal/go-setup.el
@@ -6,12 +6,11 @@
 (load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
 
 (defun my-go-mode-hook ()
+  (whitespace-mode 0)
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
-           "go build -v && go test -v && go vet")))
+           "go build -v && go test -v && go vet"))
+  )
 
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-
-
-
