6cb1d68f095d1a7de3d1080ac9253352776c9179
Eric Baur <ecb@robeybaur.org>
2017-01-12 20:44:53 -0500

    hammer whitespace-mode

--- personal/js.el
+++ personal/js.el
@@ -1,4 +1,6 @@
-(defun ecb-js-mode-hook () (setq indent-tabs-mode t tab-width 4 js-indent-level 4))
+(defun ecb-js-mode-hook ()
+  (setq indent-tabs-mode t tab-width 4 js-indent-level 4)
+  (whitespace-mode 0))
 
 (add-hook 'js-mode-hook 'ecb-js-mode-hook)
 (provide 'js)
